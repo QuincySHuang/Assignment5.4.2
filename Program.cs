8f25c357eb1fb149e1f8b7bcbfdcf6181d0429e1
Quincy <qsyhuang@gmail.com>
2025-05-12 15:55:09 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿
+//time complexity O(n^2) due to the nested for loops
+
+
+namespace Assignment5._4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("size of matrix?: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+
+            int[,] matrix = new int[size, size];
+
+            Console.WriteLine("Elements of matrix?: ");
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.WriteLine($"Element at position [{i}, {j}]");
+                    matrix[i,j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Matrix is: ");
+            for(int i = 0;i < size; i++)
+            {
+                for(int j = 0;j < size; j++)
+                {
+                    Console.Write(matrix[i,j] + " ");
+                }
+
+            }
+            int rightDiagonal = 0;
+            for(int i = 0; i < size; i++)
+            {
+                rightDiagonal = rightDiagonal + matrix[i, size - 1 -  i];
+            }
+
+            Console.WriteLine($"Sum of right diagnoal is: {rightDiagonal}");
+
+        }
+    }
+}
